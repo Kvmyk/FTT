40a65adda5785c64019248d4f78dc8e13f37582b
Kuba7331 <kuba@kamionka.eu>
2024-05-27 13:51:27 +0200

    added map function

--- serverClass.py
+++ serverClass.py
@@ -4,6 +4,7 @@ from markupsafe import escape
 from dataService import DataServiceRestaurant as Dsr
 from dbServiceClass import dbService as Dbs
 import pymysql
+import folium
 
 
 class Server():
@@ -19,78 +20,65 @@ class Server():
         app = Flask(__name__)
 
         @app.route('/')
+        def fullscreen():
+            """Simple example of a fullscreen map."""
+            m = folium.Map(tiles='http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', attr='My Data Attribution')
+            return m.get_root().render()
+        @app.route("/iframe")
+        def iframe():
+            """Embed a map as an iframe on a page."""
+            m = folium.Map()
 
-        def get_data():
+            # set the iframe width and height
+            m.get_root().width = "800px"
+            m.get_root().height = "600px"
+            iframe = m.get_root()._repr_html_()
 
-            clientData = Dsr(6, "Bar", "First Street", "Boston", "USA", "Nice place")
-            db = Dbs(clientData.dataList)
-            data = db.selectData()
+            return render_template_string(
+                """
+                    <!DOCTYPE html>
+                    <html>
+                        <head></head>
+                        <body>
+                            <h1>Using an iframe</h1>
+                            {{ iframe|safe }}
+                        </body>
+                    </html>
+                """,
+                iframe=iframe,
+            )
 
-            output = """
-    <html>
-        <head>
-            <title>Moja strona</title>
-            <style>
-                body {
-                    font-family: Arial, sans-serif;
-                    margin: 0;
-                    padding: 0;
-                    background-color: lightblue;
-                }
-                 .header {
-                    background-color: blue;
-                    color: white;
-                    padding: 10px;
-                    text-align: center;
-                }
-                .content {
-                    margin: 15px;
-                    background-color: white;
-                    padding: 10px;
-                }
-                table {
-                    width: 100%;
-                    border-collapse: collapse;
-                }
-                th, td {
-                    border: 1px solid black;
-                    padding: 10px;
-                    text-align: left;
-                }
-                th {
-                    background-color: blue;
-                    color: white;
-                } </style>
-        </head>
-        <body>
-            <div class="header">
-                <h1>Witaj na mojej stronie!</h1>
-            </div>
-            <div class="content">
-                <table>
-                    <tr>
-                        <th>Rating</th>
-                        <th>Place</th>
-                        <th>Street</th>
-                        <th>City</th>
-                        <th>Country</th>
-                        <th>Description</th>
-                    </tr>
-    """
-            for i in data:
-                rating = i[1]
-                place = i[2]
-                street = i[3]
-                city = i[4]
-                country = i[5]
-                desc = i[6]
-                output += f"<tr><td>{rating}</td><td>{place}</td><td>{street}</td><td>{city}</td><td>{country}</td><td>{desc}</td></tr>"
-            output += """
-                </table>
-            </div>
-        </body>
-    </html>
-    """    
-            return render_template_string(output)
-            
+        @app.route("/components")
+        def components():
+            """Extract map components and put those on a page."""
+            m = folium.Map(
+                width=800,
+                height=600,
+            )
+
+            m.get_root().render()
+            header = m.get_root().header.render()
+            body_html = m.get_root().html.render()
+            script = m.get_root().script.render()
+
+            return render_template_string(
+                """
+                    <!DOCTYPE html>
+                    <html>
+                        <head>
+                            {{ header|safe }}
+                        </head>
+                        <body>
+                            <h1>Using components</h1>
+                            {{ body_html|safe }}
+                            <script>
+                                {{ script|safe }}
+                            </script>
+                        </body>
+                    </html>
+                """,
+                header=header,
+                body_html=body_html,
+                script=script,
+            )
         app.run(host="0.0.0.0", port=8000, debug=True)
