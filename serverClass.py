8e5fff303d6c52494956b972853c931559cf4ed7
Kuba7331 <kuba@kamionka.eu>
2024-06-08 23:14:34 +0200

    e

--- serverClass.py
+++ serverClass.py
@@ -1,11 +1,12 @@
-from flask import Flask, jsonify, request, send_file, render_template_string
-from markupsafe import escape
-
-from dataService import DataServiceRestaurant as Dsr
-from dbServiceClass import dbService as Dbs
+import folium.plugins
 import pymysql
 import folium
 import requests
+from flask import Flask, jsonify, request, send_file, render_template_string
+from markupsafe import escape
+from dataService import DataServiceRestaurant as Dsr
+from dbServiceClass import dbService as Dbs
+from folium import plugins
 
 def get_location():
     response = requests.get('http://ip-api.com/json/')
@@ -32,63 +33,7 @@ class Server():
             """Simple example of a fullscreen map."""
             lat, lon = get_location()
             mapbox_url = "https://api.mapbox.com/styles/v1/kvmyk9/clwtnakr2010w01qs94q19z35/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoia3ZteWs5IiwiYSI6ImNsd293eGg4czEyNnEyanFlZ2lnd2Vqd2IifQ.hnwVAUb5V5WKhAKFrd0efA"
-            m = folium.Map(location=[lat,lon],tiles=mapbox_url, attr="Mapbox", zoom_start=15)
+            m = folium.Map(location=[lat, lon], zoom_start=15, width = 750, height = 500,tiles=mapbox_url, attr="Mapbox attribution", overlay = False)
             return m.get_root().render()
-        @app.route("/iframe")
-        def iframe():
-            """Embed a map as an iframe on a page."""
-            m = folium.Map()
-
-            # set the iframe width and height
-            m.get_root().width = "800px"
-            m.get_root().height = "600px"
-            iframe = m.get_root()._repr_html_()
-
-            return render_template_string(
-                """
-                    <!DOCTYPE html>
-                    <html>
-                        <head></head>
-                        <body>
-                            <h1>Using an iframe</h1>
-                            {{ iframe|safe }}
-                        </body>
-                    </html>
-                """,
-                iframe=iframe,
-            )
-
-        @app.route("/components")
-        def components():
-            """Extract map components and put those on a page."""
-            m = folium.Map(
-                width=800,
-                height=600,
-            )
-
-            m.get_root().render()
-            header = m.get_root().header.render()
-            body_html = m.get_root().html.render()
-            script = m.get_root().script.render()
-
-            return render_template_string(
-                """
-                    <!DOCTYPE html>
-                    <html>
-                        <head>
-                            {{ header|safe }}
-                        </head>
-                        <body>
-                            <h1>Using components</h1>
-                            {{ body_html|safe }}
-                            <script>
-                                {{ script|safe }}
-                            </script>
-                        </body>
-                    </html>
-                """,
-                header=header,
-                body_html=body_html,
-                script=script,
-            )
+        
         app.run(host="0.0.0.0", port=8000, debug=True)
