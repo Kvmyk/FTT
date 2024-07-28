451aa817999ccc8b7b608f5fc65dc3060422f774
Kuba7331 <kuba@kamionka.eu>
2024-07-28 20:35:13 +0200

    added toilet icon and adding new toilet ranking icon on webp

--- FTTMap.py
+++ FTTMap.py
@@ -1,5 +1,5 @@
-import folium, flask, requests
-from flask import Flask
+import folium, flask, requests, os
+from flask import Flask, send_from_directory
 
 toilet_icon = "C:\\Users\\kubak\\Desktop\\toilet_icon.png"
 
@@ -27,13 +27,44 @@ class Server():
         def fullscreen():
             lat, lon = get_location()
             m = folium.Map(location= [lat, lon],tiles= "Cartodb positron",zoom_start=15, overlay = False)
+
+            # setting up the custom icon
+
             icon = folium.CustomIcon(toilet_icon, icon_size=(50,50))
+
+            # setting the icon on map
+
             folium.Marker(location=[lat, lon], icon=icon).add_to(m)
-            return m.get_root().render()
+            m.save('map.html')
+
+            with open('map.html', 'a') as file:
+                file.write("""
+                <style>
+                .circle-plus {
+                  width: 40px;
+                  height: 40px;
+                  background-color: white;
+                  border: 3px solid red;
+                  border-radius: 50%;
+                  color: red;
+                  text-align: center;
+                  line-height: 34px;
+                  font-size: 28px;
+                  position: fixed;
+                  top: 10px;
+                  right: 10px;
+                  z-index: 1000;
+                }
+                </style>
+                <div class="circle-plus">+</div>
+                """)
+
+            return send_from_directory('.', 'map.html')
         
         app.run(host="0.0.0.0", port=8000, debug=True)
-    
-run = Server()
 
-run.runThePage()
+
+if __name__ == "__main__":   
+    run = Server()
+    run.runThePage()
 
---
+++ FTTWebpage.py
@@ -0,0 +1,4 @@
+class Webpage:
+    def __init__(self, url):
+        self.url = url
+    
