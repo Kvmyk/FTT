b03a3b63d4fa622fd9a57177977a8bfc61a6fc23
Kuba7331 <kuba@kamionka.eu>
2024-12-27 23:32:44 +0100

    xd

--- app/server.py
+++ app/server.py
@@ -43,7 +43,7 @@ class Server:
     def setup_routes(self):
         @self.app.route('/')
         def fullscreen():
-            return render_template(os.path.join('static','html','template.html'))  # Upewnij się, że używasz odpowiedniego szablonu
+            return send_from_directory('static/html', 'template.html')
 
         @self.app.route('/location', methods=['POST'])
         def location():
