a7596780be24a0664c31076fd5398588f52d3639
Kuba7331 <kuba@kamionka.eu>
2024-12-27 18:48:44 +0100

    ??

--- app/server.py
+++ app/server.py
@@ -15,7 +15,7 @@ class Server:
         self.data = None
         self.app = Flask(__name__, static_url_path='/static')
         self.app.secret_key = "twoj_sekretny_klucz"
-        self.lat, self.lon = 52.2297, 21.0122  # Default location (Warsaw, Poland)
+        self.lat, self.lon = 52.2297, 21.0122
         self.m = self.create_map()
         self.markers = self.load_markers()
         self.setup_routes()
@@ -39,85 +39,53 @@ class Server:
     def setup_routes(self):
         @self.app.route('/')
         def fullscreen():
+            # Render map with global markers
             self.save_map()
             return send_from_directory(os.path.join('static', 'html'), 'map.html')
 
         @self.app.route('/location', methods=['POST'])
         def location():
             data = request.json
-            self.lat = data['lat']
-            self.lon = data['lon']
-            session['user_location'] = {"lat": self.lat, "lon": self.lon}
-            user_marker = next((marker for marker in self.markers if marker['name'] == "User Location"), None)
+            # Store user location only in session
+            user_location = session.get('user_location', {})
+            user_location.update({
+                "lat": data['lat'],
+                "lon": data['lon'],
+                "name": "User Location",
+                "description": "This is your private location"
+            })
+            session['user_location'] = user_location
 
-            if user_marker:
-                user_marker.update({
-                    "lat": self.lat,
-                    "lon": self.lon,
-                    "description": "This is your updated location"
-                })
-            else:
-                user_marker = {
-                    "lat": self.lat,
-                    "lon": self.lon,
-                    "name": "User Location",
-                    "description": "This is your location",
-                }
-                self.markers.append(user_marker)
-            with open(os.path.join('data', 'user_location.json'), 'w', encoding='utf-8') as file:
-                json.dump(user_marker, file, ensure_ascii=False, indent=4)
-            self.update_map()
-            self.save_map()
-
-            # Aktualizacja lub dodanie markera lokalizacji użytkownika
-            user_marker = next((marker for marker in self.markers if marker['name'] == "User Location"), None)
-            if user_marker:
-                user_marker.update({
-                    "lat": self.lat,
-                    "lon": self.lon,
-                    "description": "This is your updated location"
-                })
-            else:
-                user_marker = {
-                    "lat": self.lat,
-                    "lon": self.lon,
-                    "name": "User Location",
-                    "description": "This is your location",
-                }
-                self.markers.append(user_marker)
-
-            self.save_markers()
-
-            # Aktualizuj mapę
-            self.update_map()
-
-            # Znajdź najbliższy marker i oblicz trasę
-            nearest_marker = find_nearest_marker(user_marker, self.markers)
+            # Build a map with global markers plus the user’s private pin
+            self.m = self.create_map()
+            for marker in self.markers:
+                self.add_marker_to_map(marker)
+            self.add_marker_to_map(user_location)  # Private for this user
+            # Find nearest marker from shared list, attach route
+            nearest_marker = find_nearest_marker(user_location, self.markers)
             if nearest_marker:
-                route = get_route(self.lat, self.lon, nearest_marker['lat'], nearest_marker['lon'])
+                route = get_route(user_location['lat'], user_location['lon'], nearest_marker['lat'], nearest_marker['lon'])
                 if route:
                     self.add_route_to_map(route)
-
-            # Zapisz mapę po dodaniu trasy
             self.save_map()
 
-            return jsonify({'status': 'success', 'lat': self.lat, 'lon': self.lon})
+            return jsonify({'status': 'success', 'lat': user_location['lat'], 'lon': user_location['lon']})
 
         @self.app.route('/nearest_toilet_distance', methods=['GET'])
         def nearest_toilet_distance():
-            user_marker = next((marker for marker in self.markers if marker['name'] == "User Location"), None)
-            if not user_marker:
+            user_location = session.get('user_location')
+            if not user_location:
                 return jsonify({'status': 'error', 'message': 'User location not found'}), 404
 
-            nearest_marker = find_nearest_marker(user_marker, self.markers)
+            nearest_marker = find_nearest_marker(user_location, self.markers)
             if not nearest_marker:
                 return jsonify({'status': 'error', 'message': 'No toilets found'}), 404
 
-            route = get_route(user_marker['lat'], user_marker['lon'], nearest_marker['lat'], nearest_marker['lon'])
+            route = get_route(user_location['lat'], user_location['lon'], nearest_marker['lat'], nearest_marker['lon'])
             if not route:
                 return jsonify({'status': 'error', 'message': 'Route not found'}), 404
 
-            distance = route['routes'][0]['distance']  # Długość w metrach
+            distance = route['routes'][0]['distance']
             distance_text = format_distance_text(distance)
 
             return jsonify({'status': 'success', 'distance': distance_text, 'name': nearest_marker['name']})
@@ -131,7 +99,6 @@ class Server:
             onlyForClients = data.get('onlyForClients', 'false').lower() == 'true'
             rating = data.get('rating', '0')
             photo = request.files.get('photos')
-
             photo_base64 = base64.b64encode(photo.read()).decode('utf-8') if photo else None
 
             lat, lon = get_coordinates(userInput)
@@ -143,24 +110,22 @@ class Server:
                     "description": description,
                     "payable": payable,
                     "onlyForClients": onlyForClients,
-                    "rating": rating, 
-                    "photo": photo_base64 
+                    "rating": rating,
+                    "photo": photo_base64
                 }
                 self.markers.append(new_marker)
                 self.save_markers()
-
                 self.update_map()
 
-                # **Przelicz trasę do najbliższego markera**
-                user_marker = next((marker for marker in self.markers if marker['name'] == "User Location"), None)
-                if user_marker:
-                    nearest_marker = find_nearest_marker(user_marker, self.markers)
+                # Optionally re-add user’s location
+                user_location = session.get('user_location')
+                if user_location:
+                    nearest_marker = find_nearest_marker(user_location, self.markers)
                     if nearest_marker:
-                        route = get_route(user_marker['lat'], user_marker['lon'], nearest_marker['lat'], nearest_marker['lon'])
+                        route = get_route(user_location['lat'], user_location['lon'], nearest_marker['lat'], nearest_marker['lon'])
                         if route:
                             self.add_route_to_map(route)
                 self.save_map()
-
                 return jsonify({'status': 'success', 'lat': lat, 'lon': lon})
             else:
                 return jsonify({'status': 'error', 'message': 'Location not found'})
@@ -171,7 +136,7 @@ class Server:
             return response
 
     def add_marker_to_map(self, marker):
-        if marker['name'] == "User Location":
+        if marker.get('name') == "User Location":
             icon = folium.CustomIcon(toilet_icon, icon_size=(50, 50), shadow_size=(50, 50))
             popup_content = f'''
                 <div style="width: 300px;">
@@ -203,14 +168,8 @@ class Server:
 
     def add_route_to_map(self, route):
         coordinates = [(coord[1], coord[0]) for coord in route['routes'][0]['geometry']['coordinates']]
-
-        # Oblicz całkowitą długość trasy w metrach
-        distance = route['routes'][0]['distance']  # Długość w metrach
-
-        # Sformatuj odległość do wyświetlenia
+        distance = route['routes'][0]['distance']
         distance_text = format_distance_text(distance)
-
-        # Dodaj linię trasy na mapę
         folium.PolyLine(
             locations=coordinates,
             color='red',
@@ -218,14 +177,10 @@ class Server:
             opacity=0.7
         ).add_to(self.m)
 
-        # Dodaj znacznik z długością trasy w połowie linii
         mid_point_index = len(coordinates) // 2
         mid_point = coordinates[mid_point_index]
-
-        # Dodaj przesunięcie do szerokości geograficznej, aby tekst nie nakładał się na linię
-        offset_latitude = 0.0007  # Możesz dostosować tę wartość w zależności od potrzeb
+        offset_latitude = 0.0007
         mid_point_with_offset = [mid_point[0] + offset_latitude, mid_point[1]]
-
         folium.Marker(
             location=mid_point_with_offset,
             icon=folium.DivIcon(
@@ -234,16 +189,16 @@ class Server:
         ).add_to(self.m)
 
     def update_map(self):
-        # Utwórz nową mapę
         self.m = self.create_map()
-        # Dodaj wszystkie markery
         for marker in self.markers:
             self.add_marker_to_map(marker)
+        user_location = session.get('user_location')
+        if user_location:
+            self.add_marker_to_map(user_location)
 
     def save_map(self):
         map_path = os.path.join('static', 'html', 'map.html')
         template_path = os.path.join('static', 'html', 'template.html')
-
         self.m.save(map_path)
         with open(template_path, 'r', encoding='utf-8') as template_file:
             template_content = template_file.read()
