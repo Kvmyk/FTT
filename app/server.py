4b82e1b29fde7ab62b96958e6b361bd0cbbffa5b
Kuba7331 <kuba@kamionka.eu>
2025-01-08 15:13:00 +0100

    XD

--- app/server.py
+++ app/server.py
@@ -386,8 +386,6 @@ class Server:
                                 color: #D32F2F;
                                 font-weight: bold;
                                 background-color: rgba(255, 255, 255, 0.9);
-                                padding: 0.4em 0.8em;
-                                border-radius: 0.8em;
                                 text-align: center;
                                 font-family: Arial, sans-serif;
                                 box-shadow: 0 0.15em 0.3em rgba(0,0,0,0.1);
