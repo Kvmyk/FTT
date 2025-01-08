98e7c0de6f0b8464f0b78372242e0412c3a04e84
Kuba7331 <kuba@kamionka.eu>
2025-01-08 15:08:08 +0100

    Update server.py

--- app/server.py
+++ app/server.py
@@ -382,18 +382,17 @@ class Server:
                     icon=folium.DivIcon(
                         html=f"""
                             <div style="
-                                font-size: 16px; 
+                                font-size: 14px; 
                                 color: #D32F2F;
                                 font-weight: bold;
-                                background-color: rgba(255, 255, 255, 0.95);
-                                padding: 0.6em 1em;
-                                border-radius: 0.4em;
+                                background-color: rgba(255, 255, 255, 0.9);
+                                padding: 0.8em 0.8em;
+                                border-radius: 0.3em;
                                 text-align: center;
                                 font-family: Arial, sans-serif;
-                                box-shadow: 0 0.15em 0.3em rgba(0,0,0,0.15);
+                                box-shadow: 0 0.15em 0.3em rgba(0,0,0,0.1);
                                 width: auto;
                                 white-space: nowrap;
-                                letter-spacing: 0.02em;
                             ">
                                 {distance_text}
                             </div>
