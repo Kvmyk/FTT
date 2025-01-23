fcc5c6286cb40f0d3add93c7712f3ba7fd397131
Kuba7331 <kuba@kamionka.eu>
2025-01-23 23:37:38 +0100

    Update server.py

--- app/server.py
+++ app/server.py
@@ -312,7 +312,10 @@ class Server:
             lat = marker['lat']
             lon = marker['lon']
             comment_button_html = f"""
-                <button onclick="window.parent.openCommentModal({lat}, {lon})">Dodaj komentarz</button>
+                <button onclick="window.parent.openCommentModal({lat}, {lon})" 
+                        style="width: 80%; background-color: red; color: white; padding: 14px 20px; margin: 8px 0; border: none; border-radius: 4px; cursor: pointer; font-family: 'Roboto', sans-serif; font-weight: 300;">
+                    Dodaj komentarz
+                </button>
             """
 
             wholePopUp = f"""
