6225cac89b2c27e08a4fcc1806b4579d2af8f0b6
Kuba7331 <kuba@kamionka.eu>
2025-02-04 16:04:13 +0100

    Update server.py

--- app/server.py
+++ app/server.py
@@ -339,15 +339,15 @@ class Server:
 
             navigate_button_html = f"""
                 <div>
-                    <button onclick="{f'window.parent.navigateToToilet({lat}, {lon})' if is_within_range else 'return false'}" 
+                    <button {"disabled" if not is_within_range else ""} onclick="{ 'window.parent.navigateToToilet({lat}, {lon})' if is_within_range else ''}" 
                             style="width: 80%; background-color: red; color: white; 
                                    padding: 14px 20px; margin: 8px 0; border: none; 
-                                   border-radius: 4px; cursor: {('pointer' if is_within_range else 'not-allowed')}; 
+                                   border-radius: 4px; cursor: {'pointer' if is_within_range else 'not-allowed'};
                                    font-family: 'Roboto', sans-serif; 
                                    font-weight: 300;
-                                   pointer-events: {('auto' if is_within_range else 'none')};
-                                   filter: {('none' if is_within_range else 'brightness(0.8)')};
-                                   opacity: {('1' if is_within_range else '0.7')};">
+                                   pointer-events: {'auto' if is_within_range else 'none'};
+                                   filter: {'none' if is_within_range else 'brightness(0.8)'};
+                                   opacity: {'1' if is_within_range else '0.7'};">
                         Nawiguj
                     </button>
                     {f'<span style="color: #d32f2f; font-size: 12px; margin-left: 8px;">Toaleta znajduje się dalej niż 10km</span>' if not is_within_range else ''}
