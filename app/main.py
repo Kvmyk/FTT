c7d560c9e2ec6acf5d1a7541332dde4fced6e731
Kuba7331 <kuba@kamionka.eu>
2025-02-06 17:48:44 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,11 +1,20 @@
+import ctypes
 import psutil
 import time
 import gc
 from server import Server
 
+try:
+    libc = ctypes.CDLL("libc.so.6")
+except Exception as e:
+    libc = None
+
 def monitor_memory():
     process = psutil.Process()
     while True:
+        if libc:
+            # Uwalnia nieużywaną pamięć z heapu
+            libc.malloc_trim(0)
         gc.collect()    
         mem_info = process.memory_info()
         print(f"RSS: {mem_info.rss / 1024 ** 2:.2f} MB, VMS: {mem_info.vms / 1024 ** 2:.2f} MB")
