2d0fea21f953ca575eb65db4e4cc320cbf5a8b1e
Kuba7331 <kuba@kamionka.eu>
2025-02-06 15:29:18 +0100

    xd

--- app/main.py
+++ app/main.py
@@ -6,9 +6,7 @@ def monitor_memory():
     process = psutil.Process()
     while True:
         mem_info = process.memory_info()
-        if mem_info.rss > 100 * 1024 * 1024:  # If over 100MB
-            runner.cleanup()
-        print(f"RSS: {mem_info.rss / 1024 ** 2:.2f} MB")
+        print(f"RSS: {mem_info.rss / 1024 ** 2:.2f} MB, VMS: {mem_info.vms / 1024 ** 2:.2f} MB")
         time.sleep(5)  # Sprawdzaj co 5 sekund
 
 if __name__ == "__main__":
