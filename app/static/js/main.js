8b70aaff73d1fba71567a7fb146543b399fb4de9
Kuba7331 <kuba@kamionka.eu>
2025-02-22 17:45:53 +0100

    xd

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -38,7 +38,7 @@ function sendPosition(position) {
         if (data.status === 'success') {
             const nearestPinInfo = document.getElementById('nearestPinInfo');
             const nearestPinText = document.getElementById('nearestPinText');
-            nearestPinText.innerText = `Od twojej lokalizacji do najbliższej toalety jest ${data.distance} - ${data.name}. Szacowany czas dotarcia: ${data.duration} minut.`;
+            nearestPinText.innerText = `Od twojej lokalizacji do najbliższej toalety jest ${data.distance} - ${data.name}. Szacowany czas dotarcia: ${data.duration} minut pieszo.`;
             nearestPinInfo.classList.add('show');
         }
     })
@@ -310,7 +310,7 @@ function navigateToToilet(targetLat, targetLon) {
                     if (data.status === 'success') {
                         const nearestPinInfo = document.getElementById('nearestPinInfo');
                         const nearestPinText = document.getElementById('nearestPinText');
-                        nearestPinText.innerText = `Od twojej lokalizacji do toalety jest ${data.distance} – ${data.name}. Szacowany czas dotarcia: ${data.duration} minut.`;
+                        nearestPinText.innerText = `Od twojej lokalizacji do toalety jest ${data.distance} – ${data.name}. Szacowany czas dotarcia: ${data.duration} minut pieszo.`;
                         nearestPinInfo.classList.add('show');
                     }
                 })
