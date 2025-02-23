dff57a6854d6bec1c9a20fe9c465d63a2af561d0
Kuba7331 <kuba@kamionka.eu>
2025-02-23 18:59:15 +0100

    Update main.js

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -282,10 +282,10 @@ function animateRoute(map, coordinates) {
 function isInOpoleProvince(lat, lon) {
     // Granice województwa opolskiego (przybliżone)
     const opoleBounds = {
-        north: 51.0,
-        south: 49.5,
-        west: 16.5,
-        east: 18.5
+        north: 51.1944,  // 51°11′40″ N
+        south: 49.9722,  // 49°58′20″ N
+        west: 16.9078,   // 16°54′28″ E
+        east: 18.6953    // 18°41′43″ E
     };
 
     return lat >= opoleBounds.south && lat <= opoleBounds.north &&
