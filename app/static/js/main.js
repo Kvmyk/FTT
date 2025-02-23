2f17869ca290cd727baca9cd616fb1374c369fbd
Kuba7331 <kuba@kamionka.eu>
2025-02-23 18:49:06 +0100

    Update main.js

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -51,11 +51,6 @@ function sendPosition(position) {
             const nearestPinLat = data.nearest_pin_lat;
             const nearestPinLon = data.nearest_pin_lon;
 
-            if (!isInOpoleProvince(nearestPinLat, nearestPinLon)) {
-                alert('Najbliższy marker znajduje się poza województwem opolskim. Nawigacja jest dostępna tylko do markerów w województwie opolskim.');
-                return;
-            }
-
             const nearestPinInfo = document.getElementById('nearestPinInfo');
             const nearestPinText = document.getElementById('nearestPinText');
             nearestPinText.innerText = `Od twojej lokalizacji do najbliższej toalety jest ${data.distance} - ${data.name}.\nSzacowany czas dotarcia: ${data.duration} min 🚶`;
