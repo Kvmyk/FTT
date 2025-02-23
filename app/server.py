ca3af9669ab813068731171b96a2c4fe840ca99a
Kuba7331 <kuba@kamionka.eu>
2025-02-23 19:30:45 +0100

    Update server.py

--- app/server.py
+++ app/server.py
@@ -13,7 +13,7 @@ from flask_session import Session
 from flask_compress import Compress
 
 from flask import Flask, send_from_directory, jsonify, request, session
-from utils import get_coordinates, get_route, find_nearest_marker, haversine, format_distance_text, is_hate_speech
+from utils import get_coordinates, get_route, find_nearest_marker, haversine, format_distance_text, is_hate_speech, isInOpoleProvince
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -189,12 +189,16 @@ class Server:
             # Obliczamy trasę do najbliższego markera
             nearest_marker = find_nearest_marker(user_marker, markers_to_search)
             if nearest_marker:
-                route = get_route(
-                    user_marker['lat'], user_marker['lon'],
-                    nearest_marker['lat'], nearest_marker['lon']
-                )
-                if route:
-                    self.add_route_to_map(route)
+                # Sprawdź, czy marker znajduje się w województwie opolskim
+                if isInOpoleProvince(nearest_marker['lat'], nearest_marker['lon']):
+                    route = get_route(
+                        user_marker['lat'], user_marker['lon'],
+                        nearest_marker['lat'], nearest_marker['lon']
+                    )
+                    if route:
+                        self.add_route_to_map(route)
+                else:
+                    logging.warning("Najbliższy marker jest poza województwem opolskim – nie generuję trasy.")
 
             return jsonify({
                 'status': 'success',
