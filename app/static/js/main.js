81b9ebb1b3f8c8d2217716f2a15d1ee53ebbdaa6
Kuba7331 <kuba@kamionka.eu>
2025-02-26 09:45:31 +0100

    Update main.js

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -115,7 +115,7 @@ function startIntelligentTracking() {
                         
                         // After map is updated, get distance info if we have a target
                         if (targetLat && targetLon) {
-                            return fetch('/update_user_location', {
+                            return fetch('/navigate', {
                                 method: 'POST',
                                 headers: {
                                     'Content-Type': 'application/json'
