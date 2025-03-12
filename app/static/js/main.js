eaf3771b77ea68a612ed168d46814cf078a49e9b
Kuba7331 <kuba@kamionka.eu>
2025-03-12 08:33:04 +0100

    Update main.js

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -544,6 +544,7 @@ function navigateToToilet(targetLat, targetLon) {
                         // If tracking is enabled, just restart tracking
                         stopIntelligentTracking();
                         startIntelligentTracking();
+                        window.location.reload();
                     } else {
                         // Continue with the rest of the navigation logic
                         if (data.status === 'success') {
