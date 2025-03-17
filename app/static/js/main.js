1dcdbe73705bfdd57e23869d1c637cee1b2a1a2b
Kuba7331 <kuba@kamionka.eu>
2025-03-17 16:01:26 +0100

    Update main.js

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -25,8 +25,6 @@ function startIntelligentTracking() {
         return;
     }
 
-    stopIntelligentTracking();
-
     // Sprawdź czy cel nawigacji nadal istnieje po zastosowaniu filtrów
     const targetLat = localStorage.getItem('targetLat');
     const targetLon = localStorage.getItem('targetLon');
