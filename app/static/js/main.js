34029dcbd2ecd9869a3ada9a9125fef10bd6949e
Kuba7331 <kuba@kamionka.eu>
2025-03-17 17:22:18 +0100

    Update main.js

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -545,17 +545,7 @@ function navigateToToilet(targetLat, targetLon) {
                 })
                 .then(response => response.json())
                 .then(data => {
-                    if (trackingEnabled) {
-                        // If tracking is enabled, just restart tracking
-                        stopIntelligentTracking();
-                        startIntelligentTracking();
-                        
-                    } else {
-                        // Continue with the rest of the navigation logic
-                        if (data.status === 'success') {
-                            return fetch('/render_map');
-                        }
-                    }
+                    return fetch('/render_map');
                 })
                 .then(response => response && !trackingEnabled ? response.text() : null)
                 .then(html => {
