68c1dd69cc27a6bb7d301b3f57017a7f0e961231
Kuba7331 <kuba@kamionka.eu>
2025-03-18 21:32:24 +0100

    Update main.js

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -505,7 +505,6 @@ function navigateToToilet(targetLat, targetLon) {
         // Restart śledzenia by uwzględnić nowy cel
         stopIntelligentTracking();
         startIntelligentTracking();
-        return fetch('/render_map');
     }
     
     if (navigator.geolocation) {
