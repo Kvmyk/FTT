3b6eb25e15eb8bec30d1dadc2a34ad115c8d0c79
Kuba7331 <kuba@kamionka.eu>
2025-03-30 19:02:26 +0200

    Update server.py

--- app/server.py
+++ app/server.py
@@ -1420,7 +1420,7 @@ class Server:
                 # Marker toalety (globalny)
                 iconToilet = folium.CustomIcon(
                     toilet_icon, 
-                    icon_size=(50, 55), 
+                    icon_size=(50, 69), 
                     shadow_size=(50, 60)
                 )
                 name = marker.get('name', 'Unknown')
